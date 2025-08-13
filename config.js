f21088e315a9b9cd93f2f61a01194dac7b726b73
muniamazon <muni.mudaliar1@gmail.com>
2025-08-14 06:48:54 +1000

    fixed count

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2goC95sDZOMSbv_g_WUG-0I-GwwoqMEU",
+  authDomain: "p2rboard.firebaseapp.com",
+  projectId: "p2rboard",
+  storageBucket: "p2rboard.firebasestorage.app",
+  messagingSenderId: "1099342082431",
+  appId: "1:1099342082431:web:e68ff40afe0e48cc76aa4a"
+};
